2b6b23591c4a0a0abd78c0a7275b87c8d82742e1
cap3k <37514740+cap3k@users.noreply.github.com>
2020-01-16 21:16:45 +0100

    feat: serverless function for pole emploi data

---
+++ api/data.js
@@ -0,0 +1,56 @@
+var request = require("request")
+var JSONStream = require('JSONStream')
+
+var dotenv = require('dotenv');
+
+
+dotenv.config();
+
+
+
+/* GET home page. */
+module.exports = (req, res) => {
+  console.log("new request")
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var tokenRequestOptions = {
+    method: 'POST',
+    url: process.env.POLE_EMPLOI_URL_TOKEN,
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    form: {
+      grant_type: 'client_credentials',
+      client_id: process.env.POLE_EMPLOI_CLIENT_ID,
+      client_secret: process.env.POLE_EMPLOI_CLIENT_SECRET,
+      scope: process.env.POLE_EMPLOI_SCOPE
+    }
+  };
+  var index = 0;
+  var stream = JSONStream.parse(['resultats', true, 'lieuTravail']);
+  stream.on('data', function (data) {
+    if (data.longitude && data.latitude) {
+      var lngLat = [];
+      lngLat.push(data.longitude)
+      lngLat.push(data.latitude)
+      var pos = { COORDINATES: lngLat };
+      res.write(((index === 0) ? '[' : ',') + JSON.stringify(pos));
+      index++
+    }
+  });
+  stream.on('end', function () {
+    console.log("ending response")
+    res.write(']');
+    res.end();
+  });
+
+  request(tokenRequestOptions, function (error, response, body) {
+    if (error) throw new Error(error);
+    var token = JSON.parse(body).access_token;
+
+    request.get('https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search', {
+      'auth': {
+        'bearer': token
+      }
+    }).pipe(stream)
+  })
+
+};
+
