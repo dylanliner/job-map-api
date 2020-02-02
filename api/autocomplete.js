abca2b2ec41a6c58fc260f9cb785287f1247acf6
cap3k <37514740+cap3k@users.noreply.github.com>
2020-02-02 17:33:37 +0100

    feat: cros fix

--- api/autocomplete.js
+++ api/autocomplete.js
@@ -1,5 +1,6 @@
 var communes = require('../data/communes.json')
 module.exports = (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     var reqCommune = req.query.commune
     const regex = new RegExp('^'+reqCommune, 'gi')
     var matches = communes.filter(commune => { return commune.libelle.match(regex) || commune.codePostal == reqCommune })
